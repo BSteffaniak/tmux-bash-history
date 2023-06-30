6ffb05016aabb410a0e84725f82996ccacffd8a4
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-06-30 07:30:00 -0400

    Pull TMUX_BASH_HISTORY_HOME from environment variables

--- scripts/helpers/history_helpers.sh
+++ scripts/helpers/history_helpers.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-source "$CURRENT_DIR/tmux_helpers.sh"
-
-bash_history_home_option="@bash-history-home"
 default_bash_history_home="$HOME/.bash_history.d"
-bash_history_home="$(get_tmux_option "$bash_history_home_option" "$default_bash_history_home")"
+bash_history_home=$TMUX_BASH_HISTORY_HOME
+[[ -z "$bash_history_home" ]] && bash_history_home="$default_bash_history_home"
+
+mkdir -p "$bash_history_home"
 
 function getSanitizedFileName() {
     local pane_id=$1
